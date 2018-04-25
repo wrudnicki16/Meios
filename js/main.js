93f23913982b5746745c76eab045a3a192b8a2e3
Wyatt Rudnicki <wyattrudnicki@gmail.com>
2018-04-24 17:43:07 -0700

    revert test change

--- js/main.js
+++ js/main.js
@@ -136,7 +136,6 @@ function main() {
     ctx.fillStyle = "#fff";
     ctx.fillText(`Score: ${player.score}`, 40, 50);
   }
-  // testing!
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
